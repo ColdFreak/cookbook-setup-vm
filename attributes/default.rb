36f4524bedd752a864911dddedc5d481f9363c87
Wang Zhijun <so@idcf.jp>
2016-07-06 10:42:18 +0900

    pythonの必要なパッケージをインストール

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,7 @@ default['setup-vm']['rpm-repo'] = %w{epel-release centos-release-scl}
 
 default['setup-vm']['rpm-base-packages'] = {"mlocate" => "locate", "jq" => "jq", "httpie" => "http","htop" => "htop", "etcd" => "etcdctl", "bzip2" => "bzip2", "s3cmd" => "s3cmd", "haproxy" => "haproxy"}
 
-default['setup-vm']['rpm-dev-packages'] = %w{libevent-devel make gcc-c++ curl-devel gcc openssl-devel expat-devel gettext asciidoc xmlto ncurses-devel java-1.8.0-openjdk-devel perl-ExtUtils-MakeMaker libffi-devel}
+default['setup-vm']['rpm-dev-packages'] = %w{libevent-devel make gcc-c++ curl-devel gcc openssl-devel expat-devel gettext asciidoc xmlto ncurses-devel java-1.8.0-openjdk-devel perl-ExtUtils-MakeMaker libffi-devel sqlite-devel bzip2-devel readline-devel}
 
 default['setup-vm']['tmux']['version'] = "2.2"
 default['setup-vm']['tmux']['source_url'] = "https://github.com/tmux/tmux/releases/download/#{default['setup-vm']['tmux']['version']}/tmux-#{default['setup-vm']['tmux']['version']}.tar.gz"
